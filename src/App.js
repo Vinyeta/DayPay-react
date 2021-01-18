ad6c4c6804faaf2720be19791fc882cbc77bbbab
Marcos Andaluz de Rosas <74211509+mandaluzr@users.noreply.github.com>
2021-01-18 19:24:24 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Newsletter from './components/newsletter/Newsletter';
-import React from "react";
+
 
 function App() {
   return (
     <div className="App">
+      <>
+      </>
 
-    <Newsletter />
     </div>
   );
-};
+}
 
 export default App;
