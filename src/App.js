73b0be6420179768ed114a8992ebd18cd071015c
Richard Perdomo <richard.perdomo121@gmail.com>
2021-01-20 19:47:38 +0100

    header centered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
-
-
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <>
-      </>
-
+      <Header />
     </div>
   );
 }
