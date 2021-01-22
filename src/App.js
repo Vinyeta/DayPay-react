b330428e447564a463770bfc76d16288710bc85e
Richard Perdomo <richard.perdomo121@gmail.com>
2021-01-22 19:27:05 +0100

    styles fixed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header";
+import Newsletter from "./components/newsletter/Newsletter";
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <Header />
+      <Newsletter />
     </div>
   );
 }
