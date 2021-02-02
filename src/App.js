535f2503d953c262a4dc95ad0b6bed6dac80e9e8
Felipe <f.zende@gmail.com>
2021-02-02 12:11:24 +0100

    test

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Header from "./components/Header/Header";
 import BoxInfo from "./components/BoxInfo/BoxInfo";
 import SecondPicture from "./assets/imgmorena.png"
 import TrustBox from "./components/TrustBox/TrustBox";
+<<<<<<< Updated upstream
 import IntroducePage from "./components/introducePage/IntroducePage";
 import SignUpForm from "./components/SignUpForm/signUpForm";
+=======
+import IntroducePage from "./components/introducePage/IntroducePage"
+import LoginForm from "./components/LoginForm/loginForm";
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
     <div className="App">
       <>
+<<<<<<< Updated upstream
         <SignUpForm/>
+=======
+      <LoginForm/>
+      
+>>>>>>> Stashed changes
       </>
     </div>
   );
--- src/components/LoginForm/loginForm.js
+++ src/components/LoginForm/loginForm.js
@@ -1,4 +1,12 @@
 import { useState } from "react";
+<<<<<<< Updated upstream
+=======
+import "./loginForm.css";
+import Button from "../Button/Button";
+import Logo from "../../assets/Logo.png";
+import Moreno from "../../assets/Moreno.png";
+import betterPayments from "../../assets/betterPayments.png";
+>>>>>>> Stashed changes
 
 
 
@@ -30,6 +38,7 @@ const RegisterForm = () => {
     );
   };
   return (
+<<<<<<< Updated upstream
     <div className="register__container">
     <form className="registerForm">
       <input className="label__text" placeholder="Email"
@@ -49,3 +58,38 @@ const RegisterForm = () => {
 };
 
 export default RegisterForm;
+=======
+    <div className="Login__container">
+      <div className="form__container">
+        <img src={Logo} alt="logo" className="logoDayPay"/>
+        <form className="signUpForm">
+          
+          <input className="input__container" placeholder="Email"
+            type="text"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input className="input__container" placeholder="Password"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="buttonOflogin">
+          <Button
+        style="defaultButton"
+        value="Login"/>
+            </div>
+          <span className="alreadyAccount">Don`t have an account? <a href="/login"> Sign up</a></span>
+        </form>
+      </div>
+      <div className="contenedor__imagen">
+      <img src={betterPayments} alt="betterPayments" className="imagenBetterPayments" />
+      <img src={Moreno} alt="Moreno" className="imagenMoreno" />
+            </div>
+  
+    </div>
+    );
+  };
+  
+  export default LoginForm;
+>>>>>>> Stashed changes
