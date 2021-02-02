6be1b8f005ac8e252b26e959bb3740fffa74dbaa
Felipe <f.zende@gmail.com>
2021-02-02 14:04:18 +0100

    test

--- src/components/LoginForm/loginForm.js
+++ src/components/LoginForm/loginForm.js
@@ -1,25 +1,18 @@
 import { useState } from "react";
-<<<<<<< Updated upstream
-=======
 import "./loginForm.css";
 import Button from "../Button/Button";
 import Logo from "../../assets/Logo.png";
 import Moreno from "../../assets/Moreno.png";
 import betterPayments from "../../assets/betterPayments.png";
->>>>>>> Stashed changes
 
 
 
-const RegisterForm = () => {
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
+const LoginForm = () => {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
 
 
   const body = {
-    firstName: firstName,
-    lastName: lastName,
     email: email,
     password: password,
   };
@@ -38,27 +31,6 @@ const RegisterForm = () => {
     );
   };
   return (
-<<<<<<< Updated upstream
-    <div className="register__container">
-    <form className="registerForm">
-      <input className="label__text" placeholder="Email"
-        type="text"
-        name="email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input className="label__text" placeholder="Password"
-        type="password"
-        name="password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-    </form>
-    </div>
-  );
-};
-
-export default RegisterForm;
-=======
     <div className="Login__container">
       <div className="form__container">
         <img src={Logo} alt="logo" className="logoDayPay"/>
@@ -76,20 +48,19 @@ export default RegisterForm;
           />
           <div className="buttonOflogin">
           <Button
-        style="defaultButton"
+        style="defaultButton_featured"
         value="Login"/>
             </div>
-          <span className="alreadyAccount">Don`t have an account? <a href="/login"> Sign up</a></span>
+          <span className="alreadyAccount">Don`t have an account? <a href="/signUp"> Sign up</a></span>
         </form>
       </div>
       <div className="contenedor__imagen">
+      <img src={Moreno} alt="Moreno" className="imagen__moreno" />
       <img src={betterPayments} alt="betterPayments" className="imagenBetterPayments" />
-      <img src={Moreno} alt="Moreno" className="imagenMoreno" />
-            </div>
+                  </div>
   
     </div>
     );
   };
   
   export default LoginForm;
->>>>>>> Stashed changes
