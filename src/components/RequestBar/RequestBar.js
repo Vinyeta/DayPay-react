6a3ad1fb2364025f11630d8c73cac3bc814b630d
Adrià Santos <adrsnts@gmail.com>
2021-03-08 13:53:26 +0100

    logout button

--- src/components/RequestBar/RequestBar.js
+++ src/components/RequestBar/RequestBar.js
@@ -1,8 +1,11 @@
 import "./RequestBar.css";
 import RequestBox from "../RequestBox/RequestBox";
 import Avatar from "../Avatar/Avatar";
+import { useHistory } from 'react-router-dom';
+import { ReactComponent as Forwards } from "../../assets/Forward.svg";
 
 const RequestBar = ({ user, token }) => {
+  const history = useHistory();
   return (
     <>
       <div className="RequestBar_container">
@@ -11,8 +14,13 @@ const RequestBar = ({ user, token }) => {
             <Avatar user={user} />
           </div>
           <span>
-            {user.name} {user.surname}
+            {user.name} {user.surname} <Forwards />
           </span>
+          <div className="RequestBar_UserMenu__container_dropdown">  
+          <div className="UserMenu_dropdown__list">         
+           <div style={{cursor: "pointer"}} onClick={() => {localStorage.removeItem("token"); history.replace("/login")} }>Log out</div>
+</div>
+</div>
         </div>
 
         <RequestBox user={user} token={token} />
