34dbccf61b29b712487d516a6fbc43476508dc15
Alejo <avinetam@gmail.com>
2021-03-19 20:49:32 +0100

    checking host

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -1,5 +1,6 @@
 let backendHost;
 const hostname = window && window.location && window.location.hostname;
+console.log(hostname);
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:5000';
 } else if (hostname === 'https://loving-heisenberg-90995c.netlify.app') {
