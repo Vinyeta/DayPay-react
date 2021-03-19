ec5d4f39aa4da17e57bf4b47db48afe32cfa3569
federico <federicotiralongo95@gmail.com>
2021-03-19 19:03:14 +0100

    digital oecan route added

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -3,7 +3,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:5000';
 } else if(hostname === 'app.XXXX.com') {
-    backendHost = 'https://placeholder.com';
+    backendHost = 'https://day-pay-node-2-txe3e.ondigitalocean.app/';
 }
 else backendHost = 'http://127.0.0.1:5000';
 export const API_ROOT = `${backendHost}`;
