4ea86a038d295ea0f8f0759b7ea33c120de923eb
Alejo <avinetam@gmail.com>
2021-03-19 20:54:20 +0100

    more testing

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -3,7 +3,7 @@ const hostname = window && window.location && window.location.hostname;
 console.log(hostname);
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:5000';
-} else if (hostname === 'https://loving-heisenberg-90995c.netlify.app') {
+} else if (hostname === 'loving-heisenberg-90995c.netlify.app') {
     backendHost = 'https://day-pay-node-2-txe3e.ondigitalocean.app/';
 }
 else backendHost = 'http://127.0.0.1:5000';
