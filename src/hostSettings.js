6b69c8aa6771228cc1ae0991f8bd63f5d14be1d4
Alejo <avinetam@gmail.com>
2021-03-20 09:19:06 +0100

    preparing for merge

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -1,6 +1,5 @@
 let backendHost;
 const hostname = window && window.location && window.location.hostname;
-console.log(hostname);
 if(hostname === 'localhost') {
     backendHost = 'http://127.0.0.1:5000';
 } else if (hostname === 'loving-heisenberg-90995c.netlify.app') {
