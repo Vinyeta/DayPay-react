86bc69084a3332496556453b79700dab1f814a81
Alejo <avinetam@gmail.com>
2021-03-20 09:55:28 +0100

    fixed localhost route, cleaned an unused button style

--- src/hostSettings.js
+++ src/hostSettings.js
@@ -1,10 +1,10 @@
 let backendHost;
 const hostname = window && window.location && window.location.hostname;
 if(hostname === 'localhost') {
-    backendHost = 'http://127.0.0.1:5000';
+    backendHost = 'http://127.0.0.1:5000/';
 } else if (hostname === 'loving-heisenberg-90995c.netlify.app') {
     backendHost = 'https://day-pay-node-2-txe3e.ondigitalocean.app/';
 }
-else backendHost = 'http://127.0.0.1:5000';
+else backendHost = 'http://127.0.0.1:5000/';
 export const API_ROOT = `${backendHost}`;
 export const HOSTNAME = `${hostname}`;
